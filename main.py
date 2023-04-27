6bedf91473bd8b2d5cc8212548aaa88380483a52
Eldarisuyhgur <131943652+Eldarisuyhgur@users.noreply.github.com>
2023-04-27 15:31:05 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 from scripts.main import main
+import unittest
+
+if __name__ == "__main__":
+    # Load all tests from the 'scripts/tests' package
+    suite = unittest.defaultTestLoader.discover('scripts/tests')
+
+    # Run the tests
+    unittest.TextTestRunner().run(suite)
